0bd86a48c87a2c400ee40a399693d4a2301dfcbf
dikshant2210 <dikshant2210@gmail.com>
2018-03-27 01:04:58 +0530

    test cases for DecisionTreeClassifier

---
+++ omega/test/test_dt.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2017 Mohit Rathore <mrmohitrathoremr@gmail.com>
+# Copyright (C) 2017 Dikshant Gupta <dikshant2210@gmail.com>
+# Licensed under the GNU General Public License v3.0 - https://www.gnu.org/licenses/gpl-3.0.en.html
+
+import unittest
+
+import torch
+from omega.DecisionTree import DecisionTreeClassifier
+
+import logging
+
+logging.basicConfig()
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+
+class TestNN(unittest.TestCase):
+    def setUp(self):
+        self.x = torch.Tensor([[2.771244718, 1.784783929],
+                       [1.728571309, 1.169761413],
+                       [3.678319846, 2.81281357],
+                       [3.961043357, 2.61995032],
+                       [2.999208922, 2.209014212],
+                       [7.497545867, 3.162953546],
+                       [9.00220326, 3.339047188],
+                       [7.444542326, 0.476683375],
+                       [10.12493903, 3.234550982],
+                       [6.642287351, 3.319983761]])
+        self.y = torch.Tensor([0, 0, 0, 0, 0, 1, 1, 1, 1, 1])
+
+    def model_equal(self, observed, expected):
+        self.assertTrue(torch.equal(observed, expected))
+
+    def test_model(self):
+        dt = DecisionTreeClassifier(2, 1)
+        root = dt.fit(self.x, self.y)
+
+        nodes = [root]
+        expected = [6.64229, 2.77124, 7.49755]
+        observed = list()
+        while True:
+            if isinstance(nodes[0], dict):
+                observed.append(float("{0:.5f}".format(nodes[0]['value'])))
+                nodes.append(nodes[0]['left'])
+                nodes.append(nodes[0]['right'])
+
+            nodes = nodes[1:]
+            if not nodes:
+                break
+        self.model_equal(observed, expected)
