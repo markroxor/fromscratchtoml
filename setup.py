5da35ce7b8faba67cc50d69c6a94ba3702979571
Mohit Rathore <mrmohitrathoremr@gmail.com>
2018-03-27 11:53:23 +0530

    using travis-sphinx

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     ],
     extras_require={
         'test': linux_testenv,
-        'docs': linux_testenv + ['sphinx', 'sphinxcontrib-napoleon', 'doctr', 'sphinxcontrib.programoutput']
+        'docs': linux_testenv + ['sphinx', 'sphinxcontrib-napoleon', 'travis-sphinx', 'sphinxcontrib.programoutput']
     },
     classifiers=[
         'Development Status :: Pre-Alpha',
