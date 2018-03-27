8d1c7f8a3177c0e2f528b771fe0f4505af4d035b
Mohit Rathore <mrmohitrathoremr@gmail.com>
2018-03-27 16:20:47 +0530

    fix autodoc import error

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,7 @@
 #
 import os
 import sys
-sys.path.append(os.path.abspath('.'))
+sys.path.append(os.path.abspath('../..'))
 
 # -- Project information -----------------------------------------------------
 
