7ab85cad0bc008b8638c08ecf9131dffca17cb40
Mohit Rathore <mrmohitrathoremr@gmail.com>
2018-03-27 13:31:28 +0530

    added docstrings for nn.py

--- omega/nn/nn.py
+++ omega/nn/nn.py
@@ -16,7 +16,43 @@ logger.setLevel(logging.INFO)
 
 
 class NetworkMesh(object):
+    """Objects of this class form the base of neural networks.
+
+    Examples
+    --------
+    >>> import omega as omg
+    >>> from gensim.models import TfidfModel
+    >>> from gensim.corpora import Dictionary
+    >>>
+    >>> x = ch.Tensor([[0, 0],[0, 1],[1, 0], [1, 1]])
+    >>> y = ch.Tensor([[1,0], [0,1], [0,1], [1,0]])
+    >>> train_datax = ch.utils.data.TensorDataset(x, y)
+    >>> nw = omg.nn.NetworkMesh([2, 5, 2])
+    >>> nw.SGD(train_data=train_datax, epochs=50, batch_size=4, lr=3, test_data=train_datax)
+
+    """
     def __init__(self, layer_architecture=None, seed=None):
+        """Compute tf-idf by multiplying a local component (term frequency) with a global component
+        (inverse document frequency), and normalizing the resulting documents to unit length.
+        Formula for non-normalized weight of term :math:`i` in document :math:`j` in a corpus of :math:`D` documents
+
+        .. math:: weight_{i,j} = frequency_{i,j} * log_2 \\frac{D}{document\_freq_{i}}
+
+        or, more generally
+
+        .. math:: weight_{i,j} = wlocal(frequency_{i,j}) * wglobal(document\_freq_{i}, D)
+
+        so you can plug in your own custom :math:`wlocal` and :math:`wglobal` functions.
+
+        Parameters
+        ----------
+        layer_architecture : a list of integers, optional
+            where ith integer represents the number of neurons in the ith layer
+        seed : int, optional
+            a pytorch random number seed for mantaining reproducability of weights.
+
+
+        """
         if layer_architecture is None:
             logger.warning(
                 "Network initialized without architecture definition,\
@@ -33,34 +69,102 @@ class NetworkMesh(object):
         self.layerwise_weights = [ch.randn(x, y) for x, y in zip(layer_architecture[:-1], layer_architecture[1:])]
 
     def save_model(self, file_path):
+        """This function saves the model in a file for loading it in future.
+
+        Parameters
+        ----------
+        file_path : str
+            The path to file where the model should be saved.
+
+        Returns
+        -------
+        None.
+
+        """
         ch.save(self.__dict__, file_path)
         return
 
     def load_model(self, file_path):
+        """This function loads the saved model from a file.
+
+        Parameters
+        ----------
+        file_path : str
+            The path of file from where the model should be retrieved.
+
+        Returns
+        -------
+        None.
+
+        """
         self.__dict__ = ch.load(file_path)
         return
 
     def feedforward(self, x):
+        """This function propogates the input `x` across the neural network formed
+        feed forwarding it across all the perceptrons.
+
+        Parameters
+        ----------
+        x : torch.Tensor
+            The input which is to be forwarded.
+
+        Returns
+        -------
+        x : torch.Tensor
+            The activation of the final output layer.
+
+        """
+
         x = x.view(1, ch.numel(x))
         for biases, weights in zip(self.layerwise_biases, self.layerwise_weights):
             x = sigmoid(ch.mm(x, weights) + biases)
         return x
 
-    def SGD(self, train_data, epochs, batch_size, eta, test_data=None):
+    def SGD(self, train_data, epochs, batch_size, lr, test_data=None):
+        """This function performs stochastic gradient descent on the weights,
+        for a number of epochs until we get optimum weights which return minimum
+        loss.
+
+        Parameters
+        ----------
+        train_data : list of (torch.Tensor, torch.Tensor) or a similar data type.
+            The training data on which the weights will be optimized to yield
+            minimum loss.
+        epochs : int
+            The number of times the network should be trained.
+        batch_size : int
+            The contiguous `sample` of training data after which weights should
+            be updated.
+        lr : float
+            This is the learning rate of the network, higher learning rate implies
+            higher change in weights after each updation but this might overshoot
+            the weights from their optimum values.
+        test_data : list of (torch.Tensor, torch.Tensor) or a similar data type
+                    optional
+            The test data on which the results are evaluated generally after each
+            epoch.
+
+        Returns
+        -------
+        None
+
+        """
+
         if not hasattr(self, "layer_architecture"):
             raise NotImplementedError("Define layer architecture before calling SGD.")
 
         for i in range(epochs):
             batches = ch.utils.data.DataLoader(train_data, batch_size=batch_size, shuffle=True)
             for batch in batches:
-                self.update_batch(batch, eta)
+                self.update_batch(batch, lr)
 
             logger.info("Epoch {0}:".format(i))
 
             if test_data:
                 self.evaluate(test_data), len(test_data)
 
-    def update_batch(self, batch, eta):
+    def update_batch(self, batch, lr):
         nabla_b = [ch.zeros(biases.size()) for biases in self.layerwise_biases]
         nabla_w = [ch.zeros(weights.size()) for weights in self.layerwise_weights]
 
@@ -76,12 +180,12 @@ class NetworkMesh(object):
             nabla_b = [ch.add(nb, dnb) for nb, dnb in zip(nabla_b, delta_nabla_b)]
             nabla_w = [ch.add(nw, dnw) for nw, dnw in zip(nabla_w, delta_nabla_w)]
 
-        self.update_weights(eta, nabla_b, nabla_w, len(batch))
+        self.update_weights(lr, nabla_b, nabla_w, len(batch))
 
-    def update_weights(self, eta, nabla_b, nabla_w, batch_size):
-        self.layerwise_biases = [b - (eta) * nb
+    def update_weights(self, lr, nabla_b, nabla_w, batch_size):
+        self.layerwise_biases = [b - (lr) * nb
                                  for b, nb in zip(self.layerwise_biases, nabla_b)]
-        self.layerwise_weights = [w - (eta) * nw
+        self.layerwise_weights = [w - (lr) * nw
                                  for w, nw in zip(self.layerwise_weights, nabla_w)]
 
     def backprop(self, x, y):
@@ -110,6 +214,22 @@ class NetworkMesh(object):
         return (nabla_b, nabla_w)
 
     def evaluate(self, test_data):
+        """This function evaluates the test dataset by feed forwarding the learned
+        weights across the network and calculating the number of correct evaluations
+        by the network on test data.
+
+        Parameters
+        ----------
+        test_data : list of (torch.Tensor, torch.Tensor) or a similar data type.
+            The test data on which the results are evaluated generally after each
+            epoch.
+
+        Returns
+        -------
+        None
+
+        """
+
         correct_evaluation = 0
 
         self.activations = []
