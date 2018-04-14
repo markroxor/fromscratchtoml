7e0b4db632bb9bc2a24fa3b087b19e0a3456b59a
roy <somnathroy86@gmail.com>
2018-04-15 01:34:31 +0530

    Implemented analytical, SGD and Adam for Linear Regression

--- omega/linear_regression/LinearRegressionClassifier.py
+++ omega/linear_regression/LinearRegressionClassifier.py
@@ -9,13 +9,47 @@ logger.setLevel(logging.INFO)
 
 
 class LinearRegressionClassifier(object):
+    """Objects of this class is a LinearRegressionClassifier.
+
+   Examples
+   --------
+   >>> import omega as omg
+   >>> import torch as ch
+   >>> x = ch.Tensor([[24], [50], [15], [55], [14], [12], [18], [62]])
+   >>> y = ch.Tensor([[21.54945196], [47.46446305], [17.21865634], [52.789],
+                    [16.1234], [12.789], [19.5649], [60.5793278]])
+   >>> lr = omg.linear_regression.LinearRegressionClassifier(x,y)
+   >>> lr.fit('Adam')
+   >>> lr.predict(x)
+
+   """
+
     def __init__(self, x, y):
+        """instanciate an object with two parameters.
+
+        Parameters
+        ----------
+        x : a float tensor having area of house in square meter.
+        y : a float tensor having price on the basis of area of house.
+
+        """
         self.x = x
         self.y = y
 
     def fit(self, option):
+        """ Finds suitable value of parameters which best fit the given output values.
+
+        Parameters
+        ----------
+        option : We have implemented in two ways. The first approach is the Analytical approach
+                 which computes the value of slope and intercept by finding the covariance
+                 and variance of the (x, y) pair. The second approach uses two optimizers namely
+                 stochastic gradient descent (SGD) and Adam. Therefore, the options are 'Analytical'
+                 'SGD' and 'Adam'.
+
+        """
+
         self.option = option
-        ch.manual_seed(2)
         if option == 'Analytical':
             self.cov_x_y = []
             self.var_x = []
@@ -24,58 +58,67 @@ class LinearRegressionClassifier(object):
             for i in range(len(self.x)):
                 self.cov_x_y.append((self.x[i] - mean_x) * (self.y[i] - mean_y))
                 self.var_x.append((self.x[i] - mean_x) ** 2)
-            self.b1 = sum(self.cov_x_y) / sum(self.var_x)
-            self.b0 = mean_y - self.b1 * mean_x
+            self.beta = sum(self.cov_x_y) / sum(self.var_x)
+            self.alpha = mean_y - self.beta * mean_x
+            return
+        ch.manual_seed(2)
+        x_data = Variable(ch.FloatTensor(self.x), requires_grad=False)
+        y_data = Variable(ch.FloatTensor(self.y), requires_grad=False)
+        self.beta = Variable(ch.randn(1, 1), requires_grad=True)
+        self.alpha = Variable(ch.randn(1), requires_grad=True)
+
         if option == 'SGD':
-            x_data = Variable(ch.Tensor(self.x), requires_grad=False)
-            y_data = Variable(ch.Tensor(self.y), requires_grad=False)
-            self.beta = Variable(ch.randn(1, 1), requires_grad=True)
-            self.alpha = Variable(ch.randn(1), requires_grad=True)
             optimizer = ch.optim.SGD([self.beta, self.alpha], lr=0.01)
-            for i in range(1000):
-                y_pred = x_data.mm(self.beta).add(self.alpha)
-                loss = (y_pred - y_data).pow(2).sum()
-                if i % 50 == 0:
-                    logger.info("iteration {} loss {}".format(i, loss.data[0]))
-                    optimizer.zero_grad()
-                    loss.backward()
-                    optimizer.step()
-        if option == 'Adam':
-            x_data = Variable(ch.Tensor(self.x), requires_grad=False)
-            y_data = Variable(ch.Tensor(self.y), requires_grad=False)
-            self.beta = Variable(ch.randn(1, 1), requires_grad=True)
-            self.alpha = Variable(ch.randn(1), requires_grad=True)
+        elif option == 'Adam':
             optimizer = ch.optim.Adam([self.beta, self.alpha], lr=0.01)
-            for i in range(1000):
-                y_pred = x_data.mm(self.beta).add(self.alpha)
-                loss = (y_pred - y_data).pow(2).sum()
-                if i % 50 == 0:
-                    logger.info("iteration {} loss {}".format(i, loss.data[0]))
-                    optimizer.zero_grad()
-                    loss.backward()
-                    optimizer.step()
+
+        for i in range(1000):
+            y_pred = x_data.mm(self.beta).add(self.alpha)
+            loss = (y_pred - y_data).pow(2).sum()
+            if i % 50 == 0:
+                logger.info("iteration {} loss {}".format(i, loss.data[0]))
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
 
     def predict(self, test_data):
-        if self.option == 'Analytical':
-            for i in range(len(test_data)):
-                predicted_value = self.b0 + self.b1 * test_data[i]
-                logger.info("Predicted value for test_data {} slope {} and bias {} is {}".format(test_data[i], self.b1,
-                            self.b0, predicted_value))
-        if self.option == 'SGD':
-            for i in range(len(test_data)):
-                predicted_value = self.alpha + self.beta * test_data[i]
-                logger.info("Predicted value for test_data {} slope {} and bias {} is {}".format(test_data[i],
-                            self.beta, self.alpha, predicted_value))
-        if self.option == 'Adam':
-            for i in range(len(test_data)):
-                predicted_value = self.alpha + self.beta * test_data[i]
-                logger.info("Predicted value for test_data {} slope {} and bias {} is {}".format(test_data[i],
-                            self.beta, self.alpha, predicted_value))
+            """This function evaluates the test dataset by feed forwarding the learned
+            weights across the network and calculating the number of correct evaluations
+            by the network on test data.
+
+            Parameters
+            ----------
+            test_data : list of (torch.Tensor, torch.Tensor) or a similar data type.
+                The test data on which the results are evaluated generally after each
+                epoch.
+
+            """
+            self.test_data = Variable(ch.FloatTensor(self.x), requires_grad=False)
+            for td in self.test_data:
+                    predicted_value = float(self.beta) * float(td) + float(self.alpha)
+                    logger.info("Predicted value for test_data {} slope {} and bias "
+                            "{} is {}".format(td, self.alpha, self.beta, predicted_value))
 
     def save_model(self, file_path):
+        """This function saves the model in a file for loading it in future.
+
+        Parameters
+        ----------
+        file_path : str
+            The path to file where the model should be saved.
+
+        """
         ch.save(self.__dict__, file_path)
         return
 
     def load_model(self, file_path):
+        """This function loads the saved model from a file.
+
+        Parameters
+        ----------
+        file_path : str
+            The path of file from where the model should be retrieved.
+
+        """
         self.__dict__ = ch.load(file_path)
         return
