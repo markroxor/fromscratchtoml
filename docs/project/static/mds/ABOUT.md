b13885508d9ce8c3913a547b24d4d66afe5587ef
Mohit Rathore <mrmohitrathoremr@gmail.com>
2018-04-28 00:18:08 +0530

    fixed install about

---
+++ docs/project/static/mds/ABOUT.md
@@ -0,0 +1,11 @@
+fromscratchtoml is an inefficient but intuitive machine learning library for beginners, in python.
+
+#### Shogun is accessible
+
+* Supports many languages (Python, Octave, R, Java/Scala, Lua, C#, Ruby, etc) and platforms (Linux/Unix, MacOS and Windows) and integrates with their scientific computing environments.
+* This library targets individuals who want to learn machine learning code from scratch keeping _code-readability_ and _simplicity_ over efficiency.
+
+#### fromscratchtoml is open source
+
+* fromscratchtoml is licensed under the the LGPLv3.
+* Commercially friendly LGPLv3 license
--- setup.py
+++ setup.py
@@ -2,6 +2,9 @@
 
 from setuptools import find_packages, setup
 
+with open('requirements.txt') as f:
+    install_requires = f.read().splitlines()
+
 linux_testenv = [
     'pytest-cov',
     'python-coveralls',
@@ -12,16 +15,11 @@ setup(
     version='0.0.1',
     description=' An intuitive machine learning library for python.',
 
-    install_requires=[
-        'numpy >= 1.14.2',
-        'cvxopt >= 1.2.0',
-        'matplotlib >= 2.1.0',
-    ],
+    install_requires=install_requires,
     extras_require={
         'test': linux_testenv,
-        'docs': linux_testenv + ['jupyter', 'Flask>=0.10.1', 'Jinja2>=2.7', 'MarkupSafe>=0.18', 'Werkzeug>=0.9.1',
-'               itsdangerous>=0.22', 'flask-flatpages', 'frozen-flask', 'sphinx', 'sphinxcontrib-napoleon',
-                'travis-sphinx', 'sphinxcontrib.programoutput', 'flask-assets']
+        'docs': ['jupyter', 'Flask>=0.10.1', 'Jinja2>=2.7', 'MarkupSafe>=0.18', 'Werkzeug>=0.9.1',
+'               itsdangerous>=0.22', 'flask-flatpages', 'frozen-flask', 'flask-assets']
     },
     classifiers=[
         'Development Status :: Pre-Alpha',
