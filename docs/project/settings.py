7c45748481b8b07039da5f369129f0c22dc0460b
Mohit Rathore <mrmohitrathoremr@gmail.com>
2018-04-27 22:10:34 +0530

    generate nb html from script

--- docs/project/settings.py
+++ docs/project/settings.py
@@ -2,7 +2,7 @@
 
 import os
 
-REPO_NAME = ""  # Used for FREEZER_BASE_URL
+REPO_NAME = "fromscratchtoml"  # Used for FREEZER_BASE_URL
 DEBUG = True
 
 # Assumes the app is located in the same directory
