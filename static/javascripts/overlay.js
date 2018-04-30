c5dba763ef0d0dc871be319a66e6a8d14de74f6f
Mohit Rathore <mrmohitrathoremr@gmail.com>
2018-04-30 16:10:59 +0000

    Deploy jellAIfish/fromscratchtoml to github.com/jellAIfish/fromscratchtoml.git:gh-pages

--- static/javascripts/overlay.js
+++ static/javascripts/overlay.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
     $(".overlay-iframe").attr('src', url);
     $(".overlay-new").attr('href', url);
     if (url.indexOf("notebook") != -1) {
-      var nburl="http://cloud.shogun-toolbox.org/notebook" + url.substring(url.lastIndexOf("/"), url.lastIndexOf(".")) + ".ipynb"
+      var nburl="https://github.com/jellAIfish/fromscratchtoml/tree/master/docs/notebooks" + url.substring(url.lastIndexOf("/"), url.lastIndexOf(".")) + ".ipynb"
       $(".overlay-cloud").attr('href', nburl);
     }
     $(".overlay-bg").fadeIn(500);
@@ -29,4 +29,3 @@ function closeOverlay(event) {
   $(".overlay-bg").fadeOut(500);
   $('body').css('overflow', 'visible');
 }
-
