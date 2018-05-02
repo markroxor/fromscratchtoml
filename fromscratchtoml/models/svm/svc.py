f4e9e380a56adb80a00632be43595b65ea51776a
Mohit Rathore <mrmohitrathoremr@gmail.com>
2018-05-02 17:02:52 +0530

    fix license

--- fromscratchtoml/models/svm/svc.py
+++ fromscratchtoml/models/svm/svc.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 #
 # Copyright (C) 2017 Mohit Rathore <mrmohitrathoremr@gmail.com>
-# Licensed under the GNU General Public License v3.0
+# Licensed under the GNU General Public License v3.0 - https://www.gnu.org/licenses/gpl-3.0.en.html
 
 import cvxopt
 import torch as ch
