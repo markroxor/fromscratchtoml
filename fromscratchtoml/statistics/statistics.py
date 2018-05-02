146f94fe8df9e57b238f77fc869a344f03e9102c
Mohit Rathore <mrmohitrathoremr@gmail.com>
2018-05-02 17:59:52 +0530

    comparing with sklearn

--- fromscratchtoml/statistics/statistics.py
+++ fromscratchtoml/statistics/statistics.py
@@ -16,8 +16,8 @@ class Statistics(object):
 
     @staticmethod
     def cov_matrix(X, Y):
-        x_norm = X - X.mean(dim=0)
-        y_norm = Y - Y.mean(dim=0)
+        x_norm = (X.mean(dim=0) - X)
+        y_norm = (Y.mean(dim=0) - Y)
         cov_matrix = ch.mm(x_norm.t(), y_norm) / (x_norm.size()[0] - 1)
         return cov_matrix
 
@@ -26,7 +26,7 @@ class Statistics(object):
         cov_matrix = Statistics.cov_matrix(X, X)
         # since covariance matrix is symmetric we can use linalg.eigh for efficient
         # computing
-        eigen_vals, eigen_vecs = np.linalg.eigh(cov_matrix.numpy())
+        eigen_vals, eigen_vecs = np.linalg.eig(cov_matrix.numpy())
 
         return ch.Tensor(eigen_vals), ch.Tensor(eigen_vecs)
 
@@ -36,8 +36,8 @@ class Statistics(object):
 
         sort_index = np.argsort(-eigen_vals.numpy())
         eigen_vals = eigen_vals[sort_index]
-        eigen_vecs = eigen_vecs[:, sort_index]
+        eigen_vecs = eigen_vecs[:, sort_index].t()
 
-        rescaled_x = ch.mm(X, eigen_vecs[:, :num_components])
+        rescaled_x = ch.mm(X, eigen_vecs[:, :num_components].t())
 
         return rescaled_x, eigen_vals[:num_components], eigen_vecs[:, :num_components]
