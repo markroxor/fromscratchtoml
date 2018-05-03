6f3767c72929ec89c063f43e730f21486628b670
Mohit Rathore <mrmohitrathoremr@gmail.com>
2018-05-04 01:09:17 +0530

    using only numpy

--- fromscratchtoml/decomposition/decomposition.py
+++ fromscratchtoml/decomposition/decomposition.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 # Licensed under the GNU General Public License v3.0 - https://www.gnu.org/licenses/gpl-3.0.en.html
 
-import torch as ch
 import numpy as np
 
 import logging
@@ -16,30 +15,29 @@ class Decomposition(object):
 
     @staticmethod
     def cov_matrix(X, Y):
-        x_norm = (X.mean(dim=0) - X)
-        y_norm = (Y.mean(dim=0) - Y)
-        cov_matrix = ch.mm(x_norm.t(), y_norm) / (x_norm.size()[0] - 1)
+        x_norm = (X.mean(0) - X)
+        y_norm = (Y.mean(0) - Y)
+        cov_matrix = np.dot(x_norm.T, y_norm) / (x_norm.shape[0] - 1)
         return cov_matrix
 
     @staticmethod
     def eigens(X):
         cov_matrix = Decomposition.cov_matrix(X, X)
-        eigen_vals, eigen_vecs = np.linalg.eig(cov_matrix.numpy())
+        eigen_vals, eigen_vecs = np.linalg.eig(cov_matrix)
 
-        return ch.Tensor(eigen_vals), ch.Tensor(eigen_vecs)
+        return eigen_vals, eigen_vecs
 
     @staticmethod
     def pca(X, num_components=None):
         if num_components is None:
-            num_components = X.size()[1]
+            num_components = X.shape[1]
         eigen_vals, eigen_vecs = Decomposition.eigens(X)
-        U, S, eigen_vecs = np.linalg.svd(X.numpy() - X.numpy().mean(0))
-        eigen_vecs = ch.Tensor(eigen_vecs)
+        U, S, eigen_vecs = np.linalg.svd(X - X.mean(0))
 
-        # sort_index = np.argsort(-eigen_vals.numpy())
-        # eigen_vals = eigen_vals[sort_index]
-        # eigen_vecs = eigen_vecs[:, sort_index].t()
+        sort_index = np.argsort(-eigen_vals)
+        eigen_vals = eigen_vals[sort_index]
+        eigen_vecs = eigen_vecs[sort_index]
 
-        rescaled_x = ch.mm(X, eigen_vecs.t())
+        rescaled_x = np.dot(X, eigen_vecs)
 
-        return rescaled_x, eigen_vals[:num_components], eigen_vecs  # [:, :num_components]
+        return rescaled_x, eigen_vals[:num_components], eigen_vecs[:num_components]
