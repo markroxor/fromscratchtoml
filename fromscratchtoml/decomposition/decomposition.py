4ef91af9847bb85fe5c148358958fad8b055cc5c
Mohit Rathore <mrmohitrathoremr@gmail.com>
2018-05-03 18:50:16 +0530

    using nps svd

--- fromscratchtoml/decomposition/decomposition.py
+++ fromscratchtoml/decomposition/decomposition.py
@@ -12,7 +12,7 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 
-class Statistics(object):
+class Decomposition(object):
 
     @staticmethod
     def cov_matrix(X, Y):
@@ -23,21 +23,23 @@ class Statistics(object):
 
     @staticmethod
     def eigens(X):
-        cov_matrix = Statistics.cov_matrix(X, X)
-        # since covariance matrix is symmetric we can use linalg.eigh for efficient
-        # computing
+        cov_matrix = Decomposition.cov_matrix(X, X)
         eigen_vals, eigen_vecs = np.linalg.eig(cov_matrix.numpy())
 
         return ch.Tensor(eigen_vals), ch.Tensor(eigen_vecs)
 
     @staticmethod
-    def pca(X, num_components=2):
-        eigen_vals, eigen_vecs = Statistics.eigens(X)
+    def pca(X, num_components=None):
+        if num_components is None:
+            num_components = X.size()[1]
+        eigen_vals, eigen_vecs = Decomposition.eigens(X)
+        U, S, eigen_vecs = np.linalg.svd(X.numpy() - X.numpy().mean(0))
+        eigen_vecs = ch.Tensor(eigen_vecs)
 
-        sort_index = np.argsort(-eigen_vals.numpy())
-        eigen_vals = eigen_vals[sort_index]
-        eigen_vecs = eigen_vecs[:, sort_index].t()
+        # sort_index = np.argsort(-eigen_vals.numpy())
+        # eigen_vals = eigen_vals[sort_index]
+        # eigen_vecs = eigen_vecs[:, sort_index].t()
 
-        rescaled_x = ch.mm(X, eigen_vecs[:, :num_components].t())
+        rescaled_x = ch.mm(X, eigen_vecs.t())
 
-        return rescaled_x, eigen_vals[:num_components], eigen_vecs[:, :num_components]
+        return rescaled_x, eigen_vals[:num_components], eigen_vecs  # [:, :num_components]
