ba945e0767790a70a7323b8446acc9d2849d634a
Mohit Rathore <mrmohitrathoremr@gmail.com>
2018-05-04 16:36:24 +0530

    refactoring code no folders

--- fromscratchtoml/__init__.py
+++ fromscratchtoml/__init__.py
@@ -1,3 +1,3 @@
-from fromscratchtoml.models.nn import nn # noqa:F401
+from fromscratchtoml.models import nn # noqa:F401
 from fromscratchtoml.linear_regression import LinearRegressionClassifier # noqa:F401
-from fromscratchtoml.models.svm import svc # noqa:F401
+from fromscratchtoml.models import svm # noqa:F401
