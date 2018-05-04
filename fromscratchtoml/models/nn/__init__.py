0f4147fc63e4bf8f013a9948cba89761802ebf5a
Mohit Rathore <mrmohitrathoremr@gmail.com>
2018-05-04 23:05:52 +0530

    activations with nb docs and tests

---
+++ fromscratchtoml/models/nn/__init__.py
@@ -0,0 +1,2 @@
+from .nn import NeuralNetwork  # noqa:F401
+from .activations import Activations  # noqa:F401
--- fromscratchtoml/test/nn/test_neural_networks.py
+++ fromscratchtoml/test/nn/test_neural_networks.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 #
 # Copyright (C) 2017 Mohit Rathore <mrmohitrathoremr@gmail.com>
-# Copyright (C) 2017 Dikshant Gupta <dikshant2210@gmail.com>
 # Licensed under the GNU General Public License v3.0 - https://www.gnu.org/licenses/gpl-3.0.en.html
 
 import unittest
