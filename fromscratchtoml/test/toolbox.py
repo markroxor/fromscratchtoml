c1f65762dbbdb0df9a5be85f90939b92cb4f7927
Mohit Rathore <mrmohitrathoremr@gmail.com>
2018-05-05 23:49:00 +0530

    semi numpy usage to svm

--- fromscratchtoml/test/toolbox.py
+++ fromscratchtoml/test/toolbox.py
@@ -2,13 +2,13 @@
 # -*- coding: utf-8 -*-
 #
 # Copyright (C) 2017 Mohit Rathore <mrmohitrathoremr@gmail.com>
-# Copyright (C) 2017 Dikshant Gupta <dikshant2210@gmail.com>
 # Licensed under the GNU General Public License v3.0 - https://www.gnu.org/licenses/gpl-3.0.en.html
 
 import tempfile
 import os
 
-import torch as ch
+import numpy as np
+
 pwd_path = os.path.dirname(__file__)
 
 
@@ -21,4 +21,4 @@ def _test_data_path(fname):
 
 
 def torch_equal(x, y, precision=1e-4):
-    return not ch.sum(ch.ge(ch.abs(x - y), precision))
+    return not np.sum(np.ge(np.abs(x - y), precision))
--- fromscratchtoml/toolbox/random.py
+++ fromscratchtoml/toolbox/random.py
@@ -5,7 +5,6 @@
 # Licensed under the GNU General Public License v3.0 - https://www.gnu.org/licenses/gpl-3.0.en.html
 
 import numpy as np
-import torch as ch
 
 
 class Distribution:
@@ -16,14 +15,14 @@ class Distribution:
     >>> from fromscratchtoml.toolbox.random import Distribution
     >>> import torch as ch
     >>> X1 = Distribution.linear(pts=50, mean=[8, 20], covr=[[1.5, 1], [1, 2]])
-    >>> X2 = Distribution.radial_binary(pts=50, mean=[0, 0], start=4, end=5)
+    >>> X2 = Distribution.radial_binary(pts=50, mean=[0, 0], st=4, ed=5)
 
     """
 
     @staticmethod
     def linear(pts=10,
-               mean=[0, 2],
-               covr=[[0.8, 0.6], [0.6, 0.8]],
+               mean=[0, 0],
+               covr=[[1, 0], [0, 1]],
                seed=None):
         """Returns a N-D multivariate normal distribution using mean and covariance matrix.
 
@@ -48,45 +47,50 @@ class Distribution:
         if seed:
             np.random.seed(seed)
 
-        return ch.Tensor(np.random.multivariate_normal(mean, covr, pts))
+        return np.random.multivariate_normal(mean, covr, pts)
 
     @staticmethod
-    def radial_binary(pts=100,
-               mean=[0, 0],
-               start=1,
-               end=2,
-               seed=None):
+    def radial_binary(pts=100, mean=[0, 0], st=1, ed=2,
+                      tmin=0, tmax=2 * np.pi, seed=None):
         """Returns a radial data distribution (donut shaped) from given mean and
            radial radii.
 
         Parameters
         ----------
-        pts : int
-              The number of N-D samples to be returned.
-        mean : list
-               The centre of the radial distribution.
-        start : int
-               The lower limit from which radial distribution will start to form.
-        end : int
-               The upper limit at which radial distribution will end.
+        pts : int, optional
+            The number of N-D samples to be returned.
+
+        mean : list, optional
+            The centre of the radial distribution.
+
+        st : int, optional
+            The lower limit from which radial distribution will start to form.
+        ed : int, optional
+            The upper limit at which radial distribution will end.
+
+        tmin : float, optional
+            The lower limit of theta from which radial distribution starts.
+        tmax : float, optional
+            The upper limit of theta at which radial distribution ends.
+
         seed : int, optional
                The random state seed value for mantaining reproducability of random
                number generator of numpy.
 
         Returns
         -------
-        _ : pts x 2-D torch.Tensor
+        _ : pts x 2-D numpy.ndarray
             The radial distribution.
 
         """
         if seed:
             np.random.seed(seed)
 
-        r_theta = np.random.uniform([start, 0], [end, 2 * np.pi], [pts, 2])
+        r_theta = np.random.uniform([st, tmin], [ed, tmax], [pts, 2])
 
         r = r_theta[..., 0]
         theta = r_theta[..., 1]
 
-        dist = np.stack((r * np.cos(theta), r * np.sin(theta)), axis=-1)
+        dist = np.stack((r * np.cos(theta) + mean[0], r * np.sin(theta) + mean[1]), axis=-1)
 
-        return ch.Tensor(dist)
+        return dist
