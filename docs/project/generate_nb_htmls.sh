78cd4b02390e3b7ea9aa5a38170bb7262fa527d1
Mohit Rathore <mrmohitrathoremr@gmail.com>
2018-05-06 02:18:48 +0530

    generate nb html from subdirs as well

--- docs/project/generate_nb_htmls.sh
+++ docs/project/generate_nb_htmls.sh
@@ -3,3 +3,8 @@ for f in notebooks/*.ipynb
 do
     eval "jupyter nbconvert $f --output-dir $NOTEBOOK_HTML_DIR"
 done
+
+for f in notebooks/*/*.ipynb
+do
+    eval "jupyter nbconvert $f --output-dir $NOTEBOOK_HTML_DIR"
+done
