da4a95069cb146e34e96c4ad4fb558a36e849cc8
Mohit Rathore <mrmohitrathoremr@gmail.com>
2018-07-18 23:02:03 +0530

    rnn complete

--- fromscratchtoml/neural_network/layers/rnn.py
+++ fromscratchtoml/neural_network/layers/rnn.py
@@ -8,8 +8,8 @@ import numpy as np
 
 from fromscratchtoml.neural_network.layers import Layer
 from fromscratchtoml.neural_network.activations import Activations
-import logging
 
+import logging
 import copy
 
 logging.basicConfig()
@@ -19,25 +19,19 @@ logger.setLevel(logging.INFO)
 
 class RNN(Layer):
     """
-    This is the fully connected layer where each perceptron is connected to each perceptron of the next layer.
+    This is the vanilla recurrent neural network layer.
 
     Examples
     --------
     >>> from fromscratchtoml.neural_network.models import Sequential
     >>> from fromscratchtoml.neural_network.optimizers import StochasticGradientDescent
-    >>> from fromscratchtoml.neural_network.layers import Dense, Activation
-    >>> X1 = np.array([[0, 0],[0, 1],[1, 0], [1, 1]])
-    >>> y1 = np.array([[1,0], [0,1], [0,1], [1,0]])
-    >>> model = Sequential()
-    >>> model.add(Dense(5, input_dim=2, seed=1))
-    >>> model.add(Activation('sigmoid'))
-    >>> model.add(Dense(5, seed=2))
-    >>> model.add(Activation('sigmoid'))
-    >>> model.add(Dense(2, seed=3))
-    >>> sgd = StochasticGradientDescent(learning_rate=0.1)
-    >>> model.compile(optimizer=sgd, loss="mean_squared_error")
-    >>> model.fit(X1, y1, batch_size=4, epochs=100)
-    >>> model.predict(X1, one_hot=True)
+    >>> from fromscratchtoml.neural_network.layers import RNN, Activation
+    >>> clf = Sequential()
+    >>> clf.add(RNN(units=12, memory_window=10, vocab_size=61, seed=10))
+    >>> clf.add(Activation('softmax'))
+    >>> clf.compile(optimizer=StochasticGradientDescent(learning_rate=0.00015), loss='cross_entropy')\
+    >>> clf.fit(X_train, y_train, epochs=100, batch_size=64)
+    >>> clf.predict(X_test)
     """
 
     def __init__(self, units=100, vocab_size=None, memory_window=4, seed=None):
@@ -47,9 +41,11 @@ class RNN(Layer):
         Parameters
         ----------
         units : int
-            Number of perceptrons in a layer.
-        input_dim : int, optional
-            The dimensions of the input layer. Only required for the first layer.
+            Number of hidden units of the hidden states.
+        vocab_size : int, optional
+            The size of vocabulary.
+        memory_window: The time steps before the current time step which will take part in
+            backpropogating the gradient through time.
         seed : int, optional
             The seed value for mantaining reproduciblity of random weights.
 
@@ -66,37 +62,21 @@ class RNN(Layer):
         self.memory_window = memory_window
         self.vocab_size = vocab_size
 
-        self.W_xh = np.random.uniform(-np.sqrt(1./vocab_size), np.sqrt(1./vocab_size), (self.vocab_size, self.units))
-        self.W_hh = np.random.uniform(-np.sqrt(1./units), np.sqrt(1./units), (self.units, self.units))
-        self.W_hy = np.random.uniform(-np.sqrt(1./units), np.sqrt(1./units), (self.units, self.vocab_size))
+        self.W_xh = np.random.uniform(
+                                    -np.sqrt(1. / vocab_size), np.sqrt(1. / vocab_size), (self.vocab_size, self.units))
+        self.W_hh = np.random.uniform(-np.sqrt(1. / units), np.sqrt(1. / units), (self.units, self.units))
+        self.W_hy = np.random.uniform(-np.sqrt(1. / units), np.sqrt(1. / units), (self.units, self.vocab_size))
 
-        self.W_hy = np.random.uniform(-np.sqrt(1./vocab_size), np.sqrt(1./vocab_size), (self.vocab_size, self.units))
-        self.W_xh = np.random.uniform(-np.sqrt(1./units), np.sqrt(1./units), (self.units, self.vocab_size))
+        self.params = self.W_hh.size + self.W_xh.size + self.W_hy.size
 
-        np.random.seed(10)
-
-    def to_onehot(self, X, reverse=False):
-        temp = []
-        for word in X:
-            if reverse:
-                temp.append(np.argmax(word))
-            else:
-                t = np.zeros(self.vocab_size)
-                t[word] = 1
-                temp.append(t)
-        temp = np.array(temp)
-        return temp
-
-    def forward(self, X, return_deriv=False):
+    def forward(self, X):
         """
-        Forward pass the output of the previous layer by using the current layer's weights and biases.
+        Forward pass the output of the previous layer by using the current layer's weights.
 
         Parameters
         ----------
         X : numpy.ndarray
             The ouput of the previous layer
-        return_deriv : bool, optional
-            If set to true, the function returns derivative of the output along with the output.
 
         Returns
         -------
@@ -105,109 +85,108 @@ class RNN(Layer):
         Notes
         -----
         Dimensions
-        x = time_steps(words) * vocab_size
-        xt = vocab_size
+        x = batch_size * time_steps(words) * vocab_size
+        xt = batch_size * vocab_size
         W_xh = vocab_size * units
-        z_t = x * W_xh = time_steps * units
+        z_t = x * W_xh = batch_size * time_steps * units
         W_hh = units * units
         W_hy = units * vocab_size
         ht = units
-        h = time_steps(words) * self.units
+        h = batch_size * time_steps(words) * self.units
         """
 
-        if len(X.shape) == 1:
-            X = np.expand_dims(X, axis=1)
-
-        # X = self.to_onehot(X)
+        self.time_steps = X.shape[1]
+        bs = X.shape[0]
 
         self.input = X
 
-        self.time_steps, self.vocab_size = X.shape
-        self.h = np.zeros((self.time_steps+1, self.units))
-        self.h[-1] = np.zeros(self.units)
-        self.z = np.zeros((self.time_steps+1, self.units))
-        self.z[-1] = np.zeros(self.units)
+        self.h = np.zeros((bs, self.time_steps + 1, self.units))
+        self.z = copy.copy(self.h)
 
-        self.outputs = np.zeros_like(X)
+        self.outputs = np.zeros((bs, self.time_steps, self.vocab_size))
+
         for t in range(self.time_steps):
-            self.z[t] = np.dot(X[t], self.W_xh.T) + np.dot(self.h[t - 1], self.W_hh.T)
-            self.h[t] = Activations.tanh(self.z[t])
-            y_t = np.dot(self.h[t], self.W_hy.T)
-            self.outputs[t] = y_t
 
-        # self.outputs = self.to_onehot(self.outputs, reverse=True)
-        if return_deriv:
-            return self.outputs, 0
+            # TODO do something about the large input matrix
+            self.z[:, t] = np.dot(X[:, t], self.W_xh)
+            # for ix in range(X.shape[0]):
+            #     self.z[ix, t] += self.W_xh[X[ix, t]]
+
+            self.z[:, t] += np.dot(self.h[:, t - 1], self.W_hh)
+            self.h[:, t] = Activations.tanh(self.z[:, t])
+            y_t = np.dot(self.h[:, t], self.W_hy)
+            self.outputs[:, t] = y_t
 
         return self.outputs
 
-    def back_propogate(self, delta, optimizer):
+    def back_propogate(self, dEdO):
         """
-        Backpropogate the error, this function adds the share of dense layer to the accumulated delta.
+        Backpropogate the error, this function adds the share of rnn layer to the accumulated dEdO.
 
         Parameters
         ----------
-        delta : numpy.ndarray
-            The accumulated delta used for calculating error gradient with respect to parameters.
+        dEdO : numpy.ndarray
+            The accumulated dEdO used for calculating error gradient with respect to parameters.
 
         Returns
         -------
-        numpy.array : The accumulated delta.
-        numpy.array : Current updated derivative of error with respect to bias.
-        numpy.array : Current updated derivative of error with respect to weight.
+        numpy.array : The accumulated dEdO.
 
         Notes
         -----
-        delta -> time_steps, vocab
+        dEdO -> time_steps, vocab
         h     -> time_steps, units
         """
-        if self.xh_optim is None:
-            self.xh_optim = copy.copy(optimizer)
-            self.hh_optim = copy.copy(optimizer)
-            self.hy_optim = copy.copy(optimizer)
 
-
-        dEdW_hy = np.zeros_like(self.W_hy)
-        dEdW_hh = np.zeros_like(self.W_hh)
-        dEdW_xh = np.zeros_like(self.W_xh)
-
-        accum_grad_next = np.zeros_like(delta)
-        # print("\na1", self.W_hh[0][0])
-        # print(delta, "delta")
+        self.dEdW_hy = np.zeros_like(self.W_hy)
+        self.dEdW_hh = np.zeros_like(self.W_hh)
+        self.dEdW_xh = np.zeros_like(self.W_xh)
 
         for t in reversed(range(self.time_steps)):
-            _, dhdz = Activations.tanh(self.z[t], return_deriv=True)
-            # units
-            dEdZ = np.dot(delta[t], self.W_hy) * dhdz
-            # dEdW_hy += np.outer(self.h[t], delta[t])
-            dEdW_hy += np.outer(delta[t], self.h[t])
-            accum_grad_next[t] = np.dot(dEdZ, self.W_xh)
-            # delta[t] *= np.dot(dhdz, self.W_xh.T)
+            _, dhdz = Activations.tanh(self.z[:, t], return_deriv=True)
 
+            dEdZ = np.dot(dEdO[:, t], self.W_hy.T) * dhdz
+            self.dEdW_hy += np.dot(self.h[:, t].T, dEdO[:, t])
 
-            memory_indices = np.arange(t - self.memory_window, t+1)
+            dEdO[:, t] = np.dot(dEdZ, self.W_xh.T)
+
+            memory_indices = np.arange(t - self.memory_window, t + 1)
             memory_indices = memory_indices[memory_indices >= 0]
 
-            # print(list(reversed(memory_indices)), t)
-            for tt in reversed(memory_indices):
-                dEdW_xh += np.outer(dEdZ, self.input[tt])
-                dEdW_hh += np.dot(dEdZ.T, self.h[tt-1])
-                _, dhdz = Activations.tanh(self.z[tt-1], return_deriv=True)
-                # dEdZ *= np.dot(self.W_hh, dhdz) 
-                dEdZ = np.dot(dEdZ, self.W_hh) * dhdz
+            for tt in memory_indices:
+                _, dhdz = Activations.tanh(self.z[:, tt], return_deriv=True)
 
-            # for tt in memory_indices:
-            #     _, dhdz = Activations.tanh(self.z[tt], return_deriv=True)
-            #     dEdW_xh += np.outer(self.input[tt], np.dot(self.W_hh, dhdz) * dEdZ)
-            #     dEdW_hh += np.dot(self.h[tt-1], np.dot(self.W_hh, dhdz) * dEdZ)
+                # TODO do something about the large input matrix
+                self.dEdW_xh += np.dot(self.input[:, tt].T, np.dot(dhdz, self.W_hh) * dEdZ)
+                # for ix in range(self.input.shape[0]):
+                #     self.dEdW_xh[self.input[ix, t]] += np.dot(dhdz, self.W_hh)[ix] * dEdZ[ix]
 
-            # dEdW_xh += np.outer(self.input[t], dEdZ)
-            # dEdW_hh += np.dot(dEdZ, self.h[t-1].T).T
+                self.dEdW_hh += np.dot(self.h[:, tt - 1].T, np.dot(dhdz, self.W_hh) * dEdZ)
 
-        # print(self.W_xh[0][0], self.W_hy[0][0], self.W_hh[0][0])
-        # print(dEdW_xh[0][0], dEdW_hy[0][0], dEdW_hh[0][0], "\n")
-        self.W_xh = self.xh_optim.update_weights(self.W_xh, dEdW_xh)
-        self.W_hh = self.hh_optim.update_weights(self.W_hh, dEdW_hh)
-        self.W_hy = self.hy_optim.update_weights(self.W_hy, dEdW_hy)
+            # TODO do something about the large input matrix
+            self.dEdW_xh += np.dot(self.input[:, t].T, dEdZ)
+            # for ix in range(self.input.shape[0]):
+            #     self.dEdW_xh[self.input[ix, t]] += dEdZ[ix]
 
-        return accum_grad_next
+            self.dEdW_hh += np.dot(self.h[:, t - 1].T, dEdZ)
+
+        return dEdO
+
+    def optimize(self, optimizer):
+        """
+        Updating the layer's weights.
+
+        Parameters
+        ----------
+        optimizer : fromscratchtoml.neural_network.optimizers
+            The optimizer used for updating weights.
+
+        """
+        if self.xh_optim is None:
+            self.xh_optim = copy.deepcopy(optimizer)
+            self.hh_optim = copy.deepcopy(optimizer)
+            self.hy_optim = copy.deepcopy(optimizer)
+
+        self.W_xh = self.xh_optim.update_weights(self.W_xh, self.dEdW_xh)
+        self.W_hh = self.hh_optim.update_weights(self.W_hh, self.dEdW_hh)
+        self.W_hy = self.hy_optim.update_weights(self.W_hy, self.dEdW_hy)
