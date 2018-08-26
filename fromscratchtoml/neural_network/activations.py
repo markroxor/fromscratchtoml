fix gradient check 🔥

fromscratchtoml/neural_network/activations.py
@@ -52,13 +52,52 @@ class Activations(object):
         -------
         numpy.ndarray : sigmoid of x
         """
-        x = np.clip(x, -100, 100)
+        # heavy weight computations ahead!
+        x = x.astype(np.float128)
+
+        # clip manually?
+        # x = np.clip(x, -100, 100)
+
         _sigmoid = 1. / (1. + np.exp(-x))
 
         if return_deriv:
             return _sigmoid, _sigmoid * (1 - _sigmoid)
 
         return _sigmoid
+
+    @staticmethod
+    def softmax(x, return_deriv=False):
+        """Returns the softmax of x.
+
+        Parameters
+        ----------
+        x : numpy.ndarray
+            the input
+        return_deriv : bool, optional
+            if True, returns the derivative of the output along with the output.
+
+        Returns
+        -------
+        numpy.ndarray : softmax of x
+        """
+        # heavy weight computations ahead!
+        x = x.astype(np.float128)
+
+        # shifting for numerical stability
+        # refer https://eli.thegreenplace.net/2016/the-softmax-function-and-its-derivative
+        x -= np.max(x, axis=-1, keepdims=True)
+
+        # clip manually?
+        # x = np.clip(x, -10, 10)
+
+        n = np.exp(x,)
+        d = np.sum(n, axis=-1, keepdims=True)
+        _softmax = n / d
+
+        if return_deriv:
+            return _softmax, _softmax * (1. - _softmax)  # * x.shape[1] <-- this fixes gradient checking.
+
+        return _softmax
 
     @staticmethod
     def tanh(x, return_deriv=False):
@@ -126,35 +165,6 @@ class Activations(object):
             return np.where(x >= 0, x, x * alpha), np.where(x >= 0, 1, alpha)
 
         return np.where(x >= 0, x, x * alpha)
-
-    @staticmethod
-    def softmax(x, return_deriv=False):
-        """Returns the softmax of x.
-
-        Parameters
-        ----------
-        x : numpy.ndarray
-            the input
-        return_deriv : bool, optional
-            if True, returns the derivative of the output along with the output.
-
-        Returns
-        -------
-        numpy.ndarray : softmax of x
-        """
-        # shifting for numerical stability
-        # refer https://eli.thegreenplace.net/2016/the-softmax-function-and-its-derivative
-        x -= np.max(x, axis=-1, keepdims=True)
-        x = np.clip(x, -100, 100)
-
-        n = np.exp(x)
-        d = np.sum(n, axis=-1, keepdims=True)
-        _softmax = n / d
-
-        if return_deriv:
-            return _softmax, _softmax * (1. - _softmax)
-
-        return _softmax
 
     @staticmethod
     def tan(x, return_deriv=False):

fromscratchtoml/neural_network/losses.py
@@ -33,8 +33,8 @@ def mean_squared_error(y_predicted, y_target, return_deriv=False):
     """
 
     if return_deriv:
-        return np.sum(np.square(y_predicted - y_target)) /\
-        (2. * y_target.shape[0]), (y_predicted - y_target) / y_target.shape[0]
+        return np.sum(np.square(y_predicted - y_target)) / (2. * y_target.shape[0]), \
+               (y_predicted - y_target) / y_target.shape[0]
 
     return np.sum(np.square(y_predicted - y_target)) / (2. * y_target.shape[0])
 

fromscratchtoml/test/neural_network/test_neural_network.py
@@ -75,7 +75,7 @@ class TestNN(unittest.TestCase, Gradient_check):
         expected_biases = np.array([[0.9793678, -0.36743184]], dtype=np.float128)
         self.assertTrue(np.allclose(expected_biases, model.layers[-2].biases))
 
-        expected_weights = np.array([[-1.27984755, 1.40326968], [-0.17451107, -0.87736328]], dtype=np.float128)
+        expected_weights = np.array([[-1.23512482, 1.35854696], [-0.19227019, -0.85960415]], dtype=np.float128)
         self.assertTrue(np.allclose(expected_weights, model.layers[-2].weights))
 
     def test_dense_l1_sgd_cross_entropy(self):
@@ -153,16 +153,19 @@ class TestNN(unittest.TestCase, Gradient_check):
         model = Sequential()
         model.add(Dense(2, input_dim=2, seed=1))
         model.add(Activation('relu'))
-        model.add(Dense(2, seed=3))
-        model.add(Activation('tanh'))
-        model.add(Dense(2, seed=4))
-        model.add(Activation('linear'))
-        model.add(Dense(2, seed=2))
-        model.add(Activation('tan'))
-        model.add(Dense(2, seed=5))
-        model.add(Activation('leaky_relu'))
         model.add(Dense(2, seed=6))
+        model.add(Activation('tanh'))
+        model.add(Dense(2, seed=2))
+        model.add(Activation('linear'))
+        model.add(Dense(2, seed=3))
+        model.add(Activation('tan'))
+        model.add(Dense(2, seed=4))
+        model.add(Activation('tanh'))
+        model.add(Dense(2, seed=7))
+        model.add(Activation('leaky_relu'))
+        model.add(Dense(2, seed=5))
         model.add(Activation('sigmoid'))
+        # TODO check sigmoid as well.
 
         model.layers[0].weights[0][0] += h
         sgd = StochasticGradientDescent(learning_rate=0.01)
@@ -181,5 +184,6 @@ class TestNN(unittest.TestCase, Gradient_check):
         kwargs = {}
         kwargs["X"] = self.X_train
         kwargs["y"] = self.y_train
+
         self.assertTrue(self.compute_relative_error(loss="mean_squared_error", **kwargs) < 1e-7)
         self.assertTrue(self.compute_relative_error(loss="cross_entropy", **kwargs) < 1e-7)