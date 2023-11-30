7c60def90fa0e63452380ef47bda9a98962482ea
Mohit Rathore <mrmohitrathoremr@gmail.com>
2023-11-29 20:04:07 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/demo")
 def hello_world():
     import subprocess
     subprocess.check_output("python3 demo.py", shell=1)
     return "<p>Hello, World!</p>"
+
+@app.route("/")
+def hello_world1():
+    return "<p>OK!</p>"
 if __name__ == '__main__':
    app.run(debug=True, port=8080)
